921aab6304c6115cf6263d06d0c54c2b846927b8
akashwebs <aksah.shil227@gmail.com>
2022-06-30 21:30:59 +0600

    added insert and upadate task

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import swal from 'sweetalert';
 import add from '../Assets/add.png'
-
+import { useQuery } from 'react-query';
+import Loading from './Loading';
+import { success } from 'daisyui/src/colors';
 
 const Home = () => {
+   
+    const [isCompelete,setIsCompelete]=useState(false)
+    
+    const {data: task, isLoading, refetch}=useQuery('loadTask',()=>fetch('http://localhost:5000/task').then(res=>res.json()))
+    const handleTask = (e) => {
+        e.preventDefault()
+        const taskValue = e.target.task.value;
+        if(taskValue.length>0){
+            const tasks={taskName:taskValue, isCompelete:false}
+            
+            fetch('http://localhost:5000/addTask',{
+                method:'POST',
+                headers:{
+                    'content-type':'application/json'
+                },
+                body:JSON.stringify(tasks)
+            }).then(res=>res.json())
+            .then(data=>{
+                if(data.insertedId){
+                    refetch()
+                    swal("Successfully Task Added", "", "success");
+                }
+            })
+        }
+    }
+
+    if(isLoading){
+        return <Loading></Loading>
+    }
+
+
+    
+
+
+    const handleCompelete=(id)=>{
+        
+        if(id){
+            setIsCompelete(id)
+            const url=`http://localhost:5000/updateTask/${id}`
+            fetch(url,{
+                method:"PUT",
+                headers:{
+                    'content-type':'application/json'
+                },
+                body:JSON.stringify({isCompelete:true})
+            }).then(res=>res.json())
+            .then(data=>{
+                if(data){
+                    refetch()
+                    swal('Task Completed',' ',"success")
+                }
+            })
+        }
+    }
+
+
     return (
         <div>
             <div className='flex items-center mt-12 flex-col'>
                 <h3 className='text-3xl uppercase mb-2'>Add Task</h3>
                 <div class="form-control w-full md:w-1/2">
-                    <div class="input-group ">
-                        <input type="text" placeholder="Write your task" class="input w-full text-2xl focus:outline-0 input-bordered" />
-                        <button class="btn btn-square input-bordered bg-white hover:bg-[#F44336] hover:input-bordered">
+                    <form  onSubmit={handleTask} class="input-group ">
+                        <input type="text" name='task' placeholder="Write your task" class="input w-full text-2xl focus:outline-0 input-bordered" />
+                        <button type='submit' class="btn btn-square input-bordered bg-white hover:bg-[#F44336] hover:input-bordered">
                             <img src={add} className='w-10 h-10' alt="" />
                         </button>
+                    </form>
+                </div>
+            </div>
+            <div className='px-2 md:px-16'>
+                <h2 className='text-3xl my-3 font-semibold'>My ToDo</h2>
+                <hr className='h-2' />
+                <div>
+                    <div class="overflow-x-auto">
+                        <table class="table w-full">
+
+                            <thead>
+                                <tr>
+                                    <th>Task List</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                {
+                                    task?.map(t=>{
+                                        return  <tr className='flex items-center py-3 gap-2'>
+                                        <input type="checkbox" onClick={()=>handleCompelete(t._id)} class="checkbox" /> {t.taskName}
+                                    </tr>
+                                    })
+                                }
+                                   
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
---
+++ src/Pages/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='flex h-screen justify-center items-center text-3xl'><h2>Loading...</h2></div>
+    );
+};
+
+export default Loading;
