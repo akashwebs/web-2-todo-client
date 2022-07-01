916c01aa042125f07172cac03ca4807467ff5a29
akashwebs <aksah.shil227@gmail.com>
2022-07-02 03:10:55 +0600

    final push

--- src/Pages/Calendar.js
+++ src/Pages/Calendar.js
@@ -12,7 +12,7 @@ const Calendars = () => {
 const [events, setEvents]=useState([])
  const [reFeach, setRefetch]=useState(true)
   useEffect(()=>{
-    fetch(` https://arcane-gorge-26682.herokuapp.com/${dataFormat}`)
+    fetch(`https://arcane-gorge-26682.herokuapp.com/${dataFormat}`)
     .then(res=>res.json())
     .then(data=>{
       if(data){
@@ -28,7 +28,7 @@ console.log(events)
     if(taskValue.length>0){
         const tasks={taskName:taskValue, isCompelete:false, date:dataFormat}
         
-        fetch(' https://arcane-gorge-26682.herokuapp.com/events',{
+        fetch('https://arcane-gorge-26682.herokuapp.com/events',{
             method:'POST',
             headers:{
                 'content-type':'application/json'
